ed80902b93647285eb047b5fbc16ea270c74715d
zModric <christhopher.urquiet@sansano.usm.cl>
2024-06-10 17:08:43 -0400

    panel solar
    Realice la integración del modelo de panel solar a la simulación principal del convertidor grid forming, también se dejo con el filtro diseñado. Se cambio el solver a continuo para facilitar el funcionamiento del modelo de panel solar (de todas formas solo el control del GF es necesario que opere en tiempo discreto). Se añadieron los archivos de irradiancia y mat_2L, necesarios para el funcionamiento del modelo de panel, se modificaran a posterior para configurar los datos propios necesarios.

---
+++ Simulaciones/CUV Archivos Memoria/src/mat_2L.h
@@ -0,0 +1,27 @@
+//Matrices Sx
+#define contX 8
+
+int Sx_A[contX]={0,
+1,
+1,
+0,
+0,
+0,
+1,
+1};
+int Sx_B[contX]={0,
+0,
+1,
+1,
+1,
+0,
+0,
+1};
+int Sx_C[contX]={0,
+0,
+0,
+0,
+1,
+1,
+1,
+1};
